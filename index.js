301a95f7d14d113a8a2a7a2287efadebbb924328
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-06-05 21:29:56 +0530

    g

--- index.js
+++ index.js
@@ -19,10 +19,42 @@ app.get("/users", (req, res) => {
 
 //dynamic routes
 app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-})
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+//create new user
+app.post("/api/users", (req, res) => {
+  return res.json({ status: "pending" });
+});
+
+//update user
+app.patch("/api/users/:id", (req, res) => {
+  return res.json({ status: "pending" });
+});
+
+//delete user
+app.patch("/api/users/:id", (req, res) => {
+  return res.json({ status: "pending" });
+});
+
+//you can also group the routes
+// app
+//   .route("/api/users/:id")
+//   .get((req, res) => {
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+//   })
+//   .patch((req, res) => {
+//     //edit user
+//     return res.json({ status: "pending" });
+//   })
+//   .delete((req, res) => {
+//     //delet user
+//     return res.json({ status: "pending" });
+//   });
 
 //server
 app.listen(port, () => {
